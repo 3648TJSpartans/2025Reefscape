f0b81906750275e65d673d699f675d02908a96c5
Eric Krause <eric.krause@gmail.com>
2025-01-05 12:05:45 -0700

    upgrading to 2025 minus navx

--- src/main/java/frc/robot/subsystems/MotorMMove.java
+++ src/main/java/frc/robot/subsystems/MotorMMove.java
@@ -1,15 +1,15 @@
 package frc.robot.subsystems;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.MotorConstants;
 
-
 public class MotorMMove extends SubsystemBase {
-    private final CANSparkMax newMotor1;
+    private final SparkMax newMotor1;
 
     public MotorMMove() {
-        newMotor1 = new CANSparkMax(MotorConstants.NewMotorId, MotorType.kBrushless);
+        newMotor1 = new SparkMax(MotorConstants.NewMotorId, MotorType.kBrushless);
 
     }
 
@@ -17,4 +17,3 @@ public class MotorMMove extends SubsystemBase {
         newMotor1.set(speed);
     }
 }
-
