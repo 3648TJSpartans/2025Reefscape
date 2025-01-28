2043ca0e92c63fabf5e4c0f16ce8b7dea1ebdc4e
bmalia <banksmalia1@proton.me>
2025-01-27 17:04:25 -0700

    Rewrite logroller to be more verbose and efficent
    Rewrites the logroller script to be much more verbose to the user, largely for testing purposes.
    Changes focus of the script from /u to /u/logs to keep all of the logs in one place and reduce number of files to go over

--- src/main/deploy/logroller.sh
+++ src/main/deploy/logroller.sh
@@ -9,18 +9,21 @@ two_day_old=$(date -d "2 days ago" +%s)
 date=$(date)
 
 # Setup directories and pwd
-cd /u
+cd /u/logs
 mkdir -p old-logs
-
+echo "Ready to go"
+movedcounter=0
 # Loop through each file in the source directory
-for file in /u/logs /*; do
+echo "Beginning log rotation... "
+for file in /u/logs/*.wpilog; do
   # Check if it's a file (not a directory)
   if [[ -f "$file" ]]; then
     # Get file modification time in seconds
     file_age=$(stat -c %Y "$file")
-
+    echo -n "Processing file $file..."
     # Ignore files younger than 1 day
     if [[ "$file_age" -gt "$one_day_old" ]]; then
+      echo "Left alone"
       continue
     fi
 
@@ -28,19 +31,38 @@ for file in /u/logs /*; do
     if [[ "$file_age" -le "$one_day_old" ]]; then
       # Move file to old-logs directory
       mv "$file" /u/old-logs
+      echo "Rotated"
+      movedcounter=$((movedcounter+1))
     fi
   fi
 done
-
+echo "Process complete. Rotated $movedcounter files."
+echo "Archiving old logs..."
 # Tar the old directory
 tar -czf "$date-logs.tar" old-logs
+echo "Old logs tarred as $date-logs.tar"
+echo "Compressing..."
+# gzip the tarball
 gzip "$date-logs.tar"
+echo "Old logs compressed as $date-logs.tar.gz"
+echo "Cleaning up..."
 rm "$date-old-logs.tar" # Test if this is deleted beforehand (if so this is unneeded)
 rm -r old-logs
+echo "Cleanup complete."
 
 # Remove any old log archives
-for file in /u/*.tar.gz; do
-  if [[ "$file" != "$date-logs.tar" ]]; then
+echo "Removing old log archives..."
+delcounter=0
+for file in /u/logs/*.tar.gz; do
+  echo -n "Checking tarball $file..."
+  if [[ "$file" != "$date-logs.tar.gz" ]]; then
     rm "$file"
+    echo "Deleted"
+    delcounter=$((delcounter+1))
+  else
+    echo "Left alone"
   fi
 done
+echo "Cleanup complete. Deleted $delcounter old archives."
+echo "Log management complete."
+
