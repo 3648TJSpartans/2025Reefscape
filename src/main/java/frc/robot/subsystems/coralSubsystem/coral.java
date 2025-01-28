16f9e8832c21084363c91e0dba7c439c6f8d2c9e
cheickna <hamalacompt@gmail.com>
2025-01-27 17:35:41 -0700

    fixed all the reviewed things
    i just fixed all the things that Eric told me to fix when he reviewed my code

--- src/main/java/frc/robot/subsystems/coralSubsystem/coral.java
+++ src/main/java/frc/robot/subsystems/coralSubsystem/coral.java
@@ -16,18 +16,18 @@ import edu.wpi.first.wpilibj.Encoder;
 // coral subsystem class
 public class coral extends SubsystemBase {
   /** declaration of some essential motors */
-  private SparkMax LevelMotor = new SparkMax(coralConstants.coralCANID1, MotorType.kBrushless);
-  private SparkMax takInOutMotor = new SparkMax(coralConstants.coralCANID2, MotorType.kBrushless);
-  private SparkMax AngleMotor = new SparkMax(coralConstants.coralCANID3, MotorType.kBrushless);
+  private SparkMax levelMotor = new SparkMax(coralConstants.coralCANID1, MotorType.kBrushless);
+  private SparkMax takeInOutMotor = new SparkMax(coralConstants.coralCANID2, MotorType.kBrushless);
+  private SparkMax angleMotor = new SparkMax(coralConstants.coralCANID3, MotorType.kBrushless);
   // declaration of the IR sensor
   private DigitalInput irSensor = new DigitalInput(coralConstants.irSensorPin);
   // declaration of the encoders
-  private Encoder LevelEncoder = new Encoder(coralConstants.levelChannelA, coralConstants.levelChannelB, false,
+  private Encoder levelEncoder = new Encoder(coralConstants.levelChannelA, coralConstants.levelChannelB, false,
       Encoder.EncodingType.k4X);
   private Encoder angleEncoder = new Encoder(coralConstants.angleChannelA, coralConstants.angleChannelB, false,
       Encoder.EncodingType.k4X);
-  private PIDController LevelPID = new PIDController(coralConstants.kP, coralConstants.kI, coralConstants.kD);
-  private PIDController AnglePID = new PIDController(coralConstants.angle_kP, coralConstants.angle_kI,
+  private PIDController levelPID = new PIDController(coralConstants.kP, coralConstants.kI, coralConstants.kD);
+  private PIDController anglePID = new PIDController(coralConstants.angle_kP, coralConstants.angle_kI,
       coralConstants.angle_kD);
 
   public coral() {
@@ -39,46 +39,47 @@ public class coral extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-  // this function stop the level motor
+  // this method stop the level motor
   public void stopLevelM() {
-    LevelMotor.set(0);
+    levelMotor.set(0);
   }
 
-  // if somebody see this dont forget to adjust the setting of the level method
-  // this function stop the motor that take in and out the coral
+  // this method stop the motor that take in and out the coral
   public void stopTakingM() {
-    takInOutMotor.set(0);
+    takeInOutMotor.set(0);
   }
 
   // this method stop the motor that control the angle of the coral depositor
   public void stopAngleM() {
-    AngleMotor.set(0);
+    angleMotor.set(0);
   }
 
-  // this function allows the coral subsystem to elevate itself up to a certai
+  // this method allows the coral subsystem to elevate itself up to a certain
   // height
   public void elevateTo(double setHeight) {
-    LevelMotor.set(LevelPID.calculate(GetHeight(), setHeight));
+    levelMotor.set(levelPID.calculate(getHeight(), setHeight));
   }
 
+  // this method allows the the elevator's claw to rotate up to a certain angle
   public void RotateTo(double setpoint) {
-    AngleMotor.set(AnglePID.calculate(GetAngle(), setpoint));
+    angleMotor.set(anglePID.calculate(getAngle(), setpoint));
   }
 
   // this method allows us to taking in a coral and stop when the sensor detects
   // it
   public void takeIN() {
     if (irSensor.get()) {
-      takInOutMotor.set(0);
+      takeInOutMotor.set(0);
     } else if (!irSensor.get()) {
-      takInOutMotor.set(1);// im not sure if this is 1 or -1 depending of the sense of rotation of the
-                           // mechanism
+      takeInOutMotor.set(1);// im not sure if this is 1 or -1 depending of the sense of rotation of the
+                            // mechanism
     }
   }
 
-  // this method allows us to reset the encoder
+  // this method allows us to reset the encoder used to know the level of the
+  // elevator
   public void resetLevelEn() {
-    LevelEncoder.reset();
+    levelEncoder.reset();
   }
 
   // this method allows us to rest the angle encoder
@@ -88,13 +89,13 @@ public class coral extends SubsystemBase {
 
   // this method allows us to know how height the coral subssytem have been
   // elevated
-  public double GetHeight() {
-    return LevelEncoder.getDistance();
+  public double getHeight() {
+    return levelEncoder.getDistance();
   }
 
   // this method allows us to know the angle of the intake system in the coral
   // subsytem
-  public double GetAngle() {
+  public double getAngle() {
     return angleEncoder.getDistance();
   }
 
