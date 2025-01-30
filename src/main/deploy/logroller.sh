366ba696a4be9dcac04083d99f1278ee2a1a089c
bmalia <banksmalia1@proton.me>
2025-01-29 17:59:17 -0700

    Rewrite script and add gradle postdeploy

--- src/main/deploy/logroller.sh
+++ src/main/deploy/logroller.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 logDir="/u/logs"
 archiveDir="/u/archives"
-yesterday=$( date -r $(($(date +%s) - $(($(date +%s) % 86400)))) +%s)
-yesterDate=$(date -r $(($(date +%s) - 86400)) +"%Y-%m-%d")
+today=$(date +%Y-%m-%d)
+yesterday=$(echo "$today 00:00:00" | awk '{ split($0, a, "[- :]"); print mktime(a[1] " " a[2] " " a[3] " " a[4] " " a[5] " " a[6]) }')
 
 cd $logDir
 mkdir -p oldLogs
-
 for file in $logDir/*; do
   fileAge=$(date -r $file +%s)
   if [ -f $file ]; then
@@ -16,11 +15,11 @@ for file in $logDir/*; do
   fi
 done
 
-tar -czf $archiveDir/$yesterDate-logs.tgz oldLogs
+tar -czf $archiveDir/$today-logs.tgz oldLogs
 rm -rf oldLogs
-for file in $logDir/*; do
+for file in $archiveDir/*.tgz; do
   if [ -f $file ]; then
-    if [ $file != $yesterDate-logs.tgz ]; then
+    if [ $file != $today-logs.tgz ]; then
       rm $file
     fi
   fi
