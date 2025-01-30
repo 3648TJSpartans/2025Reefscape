582032506b58f320c6ffdacb636846fea51003cc
bmalia <banksmalia1@proton.me>
2025-01-29 18:12:45 -0700

    Update to adhere to shellcheck style guide

--- src/main/deploy/logroller.sh
+++ src/main/deploy/logroller.sh
@@ -4,14 +4,14 @@ archiveDir="/u/archives"
 today=$(date +%Y-%m-%d)
 yesterday=$(echo "$today 00:00:00" | awk '{ split($0, a, "[- :]"); print mktime(a[1] " " a[2] " " a[3] " " a[4] " " a[5] " " a[6]) }')
 
-cd $logDir
+cd $logDir || exit 1
 mkdir -p oldLogs
 
-for file in $logDir/*; do
-  fileAge=$(date -r $file +%s)
-  if [ -f $file ]; then
-   if [ $fileAge -lt $yesterday ]; then
-     mv $file oldLogs
+for file in "$logDir"/*; do
+  fileAge=$(date -r "$file" +%s)
+  if [ -f "$file" ]; then
+   if [ "$fileAge" -lt "$yesterday" ]; then
+     mv "$file" oldLogs
    fi
   fi
 done
@@ -21,12 +21,12 @@ if [ -z "$(ls -A oldLogs)" ]; then
   exit 0
 fi
 
-tar -czf $archiveDir/$today-logs.tgz oldLogs
+tar -czf $archiveDir/"$today"-logs.tgz oldLogs
 rm -rf oldLogs
-for file in $archiveDir/*.tgz; do
-  if [ -f $file ]; then
-    if [ $file != $today-logs.tgz ]; then
-      rm $file
+for file in "$archiveDir"/*.tgz; do
+  if [ -f "$file" ]; then
+    if [ "$file" != "$today"-logs.tgz ]; then
+      rm "$file"
     fi
   fi
 done
