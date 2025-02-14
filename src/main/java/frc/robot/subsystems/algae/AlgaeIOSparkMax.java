ccf8c0906c5e37aa40763bf6d45ee3e117a19ce9
E-Latsis <147000359+E-Latsis@users.noreply.github.com>
2025-02-14 13:30:38 -0700

    Sequential Command Created!

--- src/main/java/frc/robot/subsystems/algae/AlgaeIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeIOSparkMax.java
@@ -14,6 +14,7 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import frc.robot.subsystems.elevator.ElevatorConstants;
+import frc.robot.util.LoggedTunableNumber;
 import frc.robot.util.TunableNumber;
 
 import com.revrobotics.spark.SparkBase.ControlType;
