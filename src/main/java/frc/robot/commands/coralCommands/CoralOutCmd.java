ace621332291933b4396090ddade82e9f0544d87
Avery Vinlove <144643328+rakdosintolerant@users.noreply.github.com>
2025-02-18 14:08:25 -0700

    code from another branch

--- src/main/java/frc/robot/commands/coralCommands/CoralOutCmd.java
+++ src/main/java/frc/robot/commands/coralCommands/CoralOutCmd.java
@@ -3,23 +3,41 @@ package frc.robot.commands.coralCommands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.coralIntake.CoralIntake;
 import frc.robot.subsystems.coralIntake.CoralIntakeConstants;
-import frc.robot.subsystems.elevator.ElevatorConstants;
+import edu.wpi.first.wpilibj.Timer;
 
 public class CoralOutCmd extends Command {
     private final CoralIntake m_coralIntake;
+    private final Timer m_timer;
+    private Boolean isFinished = false;
 
     public CoralOutCmd(CoralIntake coralIntake) {
         m_coralIntake = coralIntake;
         addRequirements(m_coralIntake);
+        m_timer = new Timer();
     }
 
     @Override
     public void initialize() {
+        m_timer.stop();
+        m_timer.reset();
     }
 
     @Override
     public void execute() {
-        m_coralIntake.setSpeed(-CoralIntakeConstants.intakeSpeed);
+        boolean objectDetected = !m_coralIntake.getIR();
+        if (objectDetected) {
+            m_coralIntake.setSpeed(-CoralIntakeConstants.intakeSpeed);
+            m_timer.reset();
+        } else {
+            if (!m_timer.hasElapsed(0.02)) {
+                if (!m_timer.isRunning()) {
+                    m_timer.start();
+                }
+                m_coralIntake.setSpeed(-CoralIntakeConstants.intakeSpeed);
+            } else {
+                isFinished = true;
+            }
+        }
     }
 
     @Override
@@ -29,7 +47,7 @@ public class CoralOutCmd extends Command {
 
     @Override
     public boolean isFinished() {
-        return !m_coralIntake.getIR();
+        return isFinished;
     }
 
 }
