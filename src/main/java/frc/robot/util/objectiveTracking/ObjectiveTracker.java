8932f3eee7036b0f485f2d3354641f255812b429
Micah Gruenwald <123602038+MicahCoder@users.noreply.github.com>
2025-02-24 17:54:25 -0700

    Update Objective Tracker

--- src/main/java/frc/robot/util/objectiveTracking/ObjectiveTracker.java
+++ src/main/java/frc/robot/util/objectiveTracking/ObjectiveTracker.java
@@ -22,10 +22,82 @@ public class ObjectiveTracker {
     private Pose2d[] rightReefPoints = PoseConstants.rightReefPoints;
     private Pose2d[] leftReefPoints = PoseConstants.leftReefPoints;
     private Pose2d Defualt_A = rightReefPoints[0];
-    private Pose2d Defualt_B = rightReefPoints[0];
+    private Pose2d Defualt_B = leftReefPoints[0];
+    private Pose2d Defualt_C = rightReefPoints[1];
+    private Pose2d Defualt_D = leftReefPoints[1];
+    private Pose2d Defualt_E = rightReefPoints[2];
+    private Pose2d Defualt_F = leftReefPoints[2];
+    private Pose2d Defualt_G = rightReefPoints[3];
+    private Pose2d Defualt_H = leftReefPoints[3];
+    private Pose2d Defualt_I = rightReefPoints[4];
+    private Pose2d Defualt_J = leftReefPoints[4];
+    private Pose2d Defualt_K = rightReefPoints[5];
+    private Pose2d Defualt_L = leftReefPoints[5];
+
+    private ObjectiveTrackerObject[] defualtObjectives;
 
     public ObjectiveTracker() {
+        defualtObjectives = new ObjectiveTrackerObject[] {
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 1),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 2),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 3),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 4),
 
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 1),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 2),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 3),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 4),
+
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 1),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 2),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 3),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 4),
+
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 1),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 2),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 3),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 4),
+
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 1),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 2),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 3),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 4),
+
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 1),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 2),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 3),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 4),
+
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 1),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 2),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 3),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 4),
+
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 1),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 2),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 3),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 4),
+
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 1),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 2),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 3),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 4),
+
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 1),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 2),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 3),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 4),
+
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 1),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 2),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 3),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 4),
+
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 1),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 2),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 3),
+                new ObjectiveTrackerObject(Defualt_A, false, Reefpoint.A, 4)
+        };
     }
 
 }
