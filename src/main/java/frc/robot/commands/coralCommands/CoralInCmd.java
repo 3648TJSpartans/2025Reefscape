08661a2a5f93fb4627bcf8428c99a178145f28da
Avery Vinlove <144643328+rakdosintolerant@users.noreply.github.com>
2025-02-26 14:19:06 -0700

    updates

--- src/main/java/frc/robot/commands/coralCommands/CoralInCmd.java
+++ src/main/java/frc/robot/commands/coralCommands/CoralInCmd.java
@@ -4,15 +4,18 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.coralIntake.CoralIntake;
 import frc.robot.subsystems.coralIntake.CoralIntakeConstants;
+import frc.robot.subsystems.elevator.Elevator;
 
 public class CoralInCmd extends Command {
     private final CoralIntake m_coralIntake;
+    private final Elevator m_elevator;
     private final Timer m_timer;
     private Boolean isFinished = false;
 
-    public CoralInCmd(CoralIntake coralIntake) {
+    public CoralInCmd(CoralIntake coralIntake, Elevator elevator) {
         m_coralIntake = coralIntake;
-        addRequirements(m_coralIntake);
+        m_elevator = elevator;
+        addRequirements(m_coralIntake, m_elevator);
         m_timer = new Timer();
     }
 
@@ -24,10 +27,15 @@ public class CoralInCmd extends Command {
 
     @Override
     public void execute() {
-        boolean objectDetected = !m_coralIntake.getIR();
-        if (!objectDetected) {
+        boolean coralInSFT = !m_coralIntake.getIR();
+        boolean coralInArm = !m_coralIntake.getIR();
+        if (coralInSFT) {
             m_coralIntake.setSpeed(CoralIntakeConstants.intakeSpeed);
-            m_timer.reset();
+            m_elevator.set
+            if (!coralInArm) {
+                m_timer.reset();
+
+            }
         } else {
             if (!m_timer.hasElapsed(0.1)) {
                 if (!m_timer.isRunning()) {
