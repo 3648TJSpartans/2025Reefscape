3bd321558d9ac81240de53a7efeea94a4607296b
MicahCoder <123602038+MicahCoder@users.noreply.github.com>
2025-03-01 14:53:41 -0700

    Commit (#98)

--- src/main/java/frc/robot/subsystems/sft/SftConstants.java
+++ src/main/java/frc/robot/subsystems/sft/SftConstants.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems.sft;
 
 public class SftConstants {
-    public static final int SftMotorPin = 15;// TODO update placeholder
+    public static final int SftMotorPin = 14;// TODO update placeholder
     public static final int irSensorPin = 3;// TODO update placeholder
 
     public static final double kP = .5;
