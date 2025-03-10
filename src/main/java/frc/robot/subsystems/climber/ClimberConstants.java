558528eee4d7181d92a2357a5d916eb3646b87f5
Avery Vinlove <144643328+rakdosintolerant@users.noreply.github.com>
2025-03-10 17:06:44 -0600

    Climber analog limited (#104)
    * initial commit
    
    * now only goes one way
    
    * added setpoints
    
    * initial commit
    
    * now only goes one way
    
    * added setpoints
    
    * fixed constants for climber limits

--- src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
@@ -31,8 +31,11 @@ public class ClimberConstants {
     public static final boolean followEncoderInverted = false;
     public static final double followEncoderPositionFactor = 1;
 
-    public static final double upPosition1 = 0.134; // change later //left
-    public static final double upPosition2 = 0.004; // change later //right
+    public static final double upPosition1 = 0.025;
+    public static final double upPosition2 = 0.08;
+
+    public static final double downPosition1 = 1;
+    public static final double downPosition2 = 0.8; // may need testing
     // These might need flipping
 
 }
